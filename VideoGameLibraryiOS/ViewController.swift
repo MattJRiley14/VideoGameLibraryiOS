b3f06b804f96dd3fe16c28801430b118bce51dc6
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-10-15 12:41:07 -0500

    Initial Commit

---
+++ VideoGameLibraryiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoGameLibraryiOS
+//
+//  Created by Matthew Riley on 10/15/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
